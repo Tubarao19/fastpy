9564de2bd8814a15df1bc358cd45962052d7d32e
tubarao19 <miguelmonsalvo19@gmail.com>
2024-06-21 18:53:28 -0500

    metodo get

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,4 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return 'Hola fastapi'
+
+@app.get("/url")
+async def root():
+    return { "url_curso":"https://www.udemy.com/" }
 
