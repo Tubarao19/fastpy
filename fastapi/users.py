c3bc17dfd0fd65c32a65036b768f9626dc198d7d
tubarao19 <miguelmonsalvo19@gmail.com>
2024-07-16 02:18:29 -0500

    inicio de api

--- fastapi/users.py
+++ fastapi/users.py
@@ -3,16 +3,22 @@ from pydantic import BaseModel # type: ignore
 
 app = FastAPI()
 
-class Users(BaseModel):
+#entidad usuario
+
+class User(BaseModel):
 	name: str
 	surname: str
 	url: str
 	age: int
 
-#entidad que se usa como base de datos de prueba
-users_list = [Users(name= 'Brais', surname= 'moure', url= 'https://www.animefenix.tv/zerotwo', age= 35)]
+#entidad que se usa como base de datos de prueba, construccion de los datos con base en la clase
+users_list = [User(name= 'Brais', surname= 'moure', url= 'https://www.animefenix.tv/zerotwo', age= 35),
+              User(name= 'Hakuna', surname= 'matata', url= 'https://www.animefenix.tv/zerotwo', age= 40),
+              User(name= 'Brais2', surname= 'moure2', url= 'https://www.animefenix.tv/zerotwo', age= 38)]
 
+#paginas a las que se podra ingresar
 
+#construccion de los datos a mano
 @app.get("/usersjson")
 async def usersjson():
     return [{'name': 'Brais', 'surname': 'moure', 'url': 'https://www.animefenix.tv/zerotwo', 'age': 35},
